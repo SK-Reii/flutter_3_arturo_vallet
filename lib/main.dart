27a17b7ca63ae6386ff8988440c93e7a2ab05b82
Rei <visatuto@gmail.com>
2023-10-23 17:21:45 +0200

    Layouts

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, World!"),
-      ), // AppBar
-    ), // Scaffold
-  )); // MaterialApp
+      appBar: AppBar (title: Text("Hello, World! Arturo")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container (
+            decoration: BoxDecoration (
+            color: Colors.red,
+            ),
+            child: Text("Uno"),
+          ),
+          Container(
+            decoration: BoxDecoration (
+            color: Colors.green,
+            ),
+            child: Text("Dos"),
+          ),
+          Container (
+            decoration: BoxDecoration (
+              color: Colors.blue,
+            ),
+            child: Text("Tres"),
+          ),
+        ],
+      ))));
 }
